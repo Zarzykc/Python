81591fbf8d371075af556d8138b9f1aa01fb71de
Zarzykc <adam.zarzycki19@gmail.com>
2022-05-10 22:43:39 +0200

    updates

--- break.py
+++ break.py
@@ -1,6 +1,6 @@
 #break
 
-wynik = 0
+"""wynik = 0
 
 for i in range(3):
     x = int(input("Podaj liczbę dodatnią: "))
@@ -10,3 +10,26 @@ for i in range(3):
         print("Dodatnia!")
         continue
     print("Aktualny wynik dodawania to: ", wynik)
+    """
+
+
+
+"""for i in range(10):
+    if (i % 2 == 0):
+        print(i)
+    
+for j in range(15):
+    if (j % 2 == 1):
+        print(j)    
+"""
+
+a = int(input("1 liczba to: "))
+b = int(input("2 liczba to: "))
+
+wynik = 0
+
+if (a >= 0 and b < 10):
+    print(a + b)   
+elif (a < 0 and b < 0):
+    print(a + b)
+    
--- pobdan.py
+++ pobdan.py
@@ -1,6 +1,21 @@
-print("Kalkulator, który tylko dodaje")
 
-a = int(input("Podaj pierwszą liczbę: "))
-b = int(input("Podaj drugą liczbę: "))
+Dzialanie = int(input("Wybierz działanie: 1 - dodawanie, 2 - odejmowanie, 3 - mnożenie, 4 - dzielenie, 5 - potegowanie, 6 - modulo(Reszta z dzielenia) "))
 
-print("Wynik dodawania", a, "+", b, "to:", a + b)
+if (Dzialanie >= 1 and Dzialanie <= 6):
+        a = int(input("Podaj liczbe 1: "))
+        b = int(input("Podaj liczbę 2: "))
+else:
+        print("Zly wybor")
+
+if (Dzialanie == 1):
+    print(a + b)
+elif (Dzialanie == 2):
+    print(a - b)
+elif (Dzialanie == 3):
+    print(a * b)
+elif (Dzialanie == 4):
+    print(a / b)
+elif (Dzialanie == 5):
+    print(a ** b)
+elif (Dzialanie == 6):
+    print(a % b)
